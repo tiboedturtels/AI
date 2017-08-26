98a377799a8175c75fde185a5062336add5d0ede
garnes <garnes@lab2-12.eng.utah.edu>
2017-08-26 14:06:00 -0600

    set up files

---
+++ A1/Matlab/A1_Script.m
@@ -0,0 +1,3 @@
+
+t = CS4300_WW2(50,'CS4300_agent1');
+M = CS4300_show_trace(t,1);
---
+++ A1/Matlab/CS4300_agent1.m
@@ -0,0 +1,40 @@
+function action = CS4300_agent1(percept)
+% CS4300_Example1 - simple agent example
+%    Takes actions: right, left, climb
+% On input:
+%     percept (1x5 Boolean vector): percept values
+%      (1): Stench
+%      (2): Breeze
+%      (3): Glitters
+%      (4): Bumped
+%      (5): Screamed
+% On output:
+%     action (int): action selected by agent
+%       FORWARD = 1;
+%       ROTATE_RIGHT = 2;
+%       ROTATE_LEFT = 3;
+%       GRAB = 4;
+%       SHOOT = 5;
+%       CLIMB = 6;
+% Call:
+%     a = CS4300_Example1([0,1,0,0,0]);
+% Author:
+%     T. Henderson
+%     UU
+%     Summer 2015
+%
+
+persistent state
+
+FORWARD = 1;
+ROTATE_RIGHT = 2;
+ROTATE_LEFT = 3;
+GRAB = 4;
+SHOOT = 5;
+CLIMB = 6;
+
+action = ROTATE_RIGHT;
+
+if isempty(state)
+    state = 0;
+end
