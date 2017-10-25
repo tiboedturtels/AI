240172c257f73bee93d8e2756092e58e668bb558
William Alexander Garnes III <wagarnes@gmail.com>
2017-10-24 18:34:18 -0600

    ask file changed (not working)

--- A4/MatLab/CS4300_Ask.m
+++ A4/MatLab/CS4300_Ask.m
@@ -1,45 +1,47 @@
-function b = CS4300_Ask(KB,sentence) 
-% CS4300_Ask - Ask function for logic KB 
-% On input: 
-% KB (KB struct): Knowledge base (CNF) 
-% (k).clauses (1xp vector): disjunction clause 
-% sentence (KB struct): query theorem (CNF) 
-% (k).clauses (1xq vector): disjunction 
-% On output: 
-% b (Boolean): 1 if KB entails sentence, else 0 
-% Call: 
-% KB(1).clauses = [1]; 
-% KB(2).clauses = [-1,2]; 
-% sentence(1).clauses = [2]; 
-% b = CS4300_Ask(KB,sentence); 
-% Author: 
-% William Garnes and Cameron Jackson 
-% UU 
-% Fall 2017 
+function b = CS4300_Ask(KB,sentence)
+% CS4300_Ask - Ask function for logic KB
+% On input:
+%     KB (KB struct): Knowledge base (CNF)
+%       (k).clauses (1xp vector): disjunction clause
+%     sentence (KB struct): query theorem (CNF)
+%       (k).clauses (1xq vector): disjunction
+% On output:
+%     b (Boolean): 1 if KB entails sentence, else 0
+% Call:
+%     KB(1).clauses = [1];
+%     KB(2).clauses = [-1,2];
+%     sentence(1).clauses = [2];
+%     b = CS4300_Ask(KB,sentence);
+% Author:
+%     T. Henderson
+%     UU
+%     Fall 2017
 %
 
-kb_size = length(KB);
-vars = [];
+b = 0;
 
-%add variables in the kb to vars
-for index = 1 : kb_size
-    kb_vars = abs(KB(index).clauses);
-    vars = union(vars, kb_vars);
+if isempty(sentence)
+    return
 end
 
-sentence_size = length(sentence);
-true_count = 0;
-
-for index = 1 : sentence_size
-   sent = sentence(index).clauses;
-   sentence_vars = abs(sent);
-   temp_vars = union(vars, sentence_vars);
-   Sip = CS4300_RTP(KB, sent, temp_vars);
-   if(isempty(Sip))
-       true_count = true_count + 1;
-   else
-       break
-   end
+%vars = CS4300_get_vars(KB,sentence);
+vars = [1];
+num_sentences = length(KB);
+len_sentence = length(sentence);
+for s = 1:len_sentence
+    KB(num_sentences+1).clauses = -sentence(s).clauses;
+    KB(num_sentences+1).clauses
+    CS4300_create_SAT_prob(KB,'HYBKB');
+    system('sat.py < HYBKB >popo');
+    fd = fopen('popo','r');
+    t = fscanf(fd,'%s');
+    if ~isempty(t)
+        return
+    else
+        t
+    end
+    clear t
+    fclose(fd);
 end
 
-b = true_count == sentence_size;
+b = 1;
