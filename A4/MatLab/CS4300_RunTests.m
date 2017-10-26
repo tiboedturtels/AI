7147484afad6778de6767f9cfc77035dafb50b9e
tiboedturtels <u0927597@utah.edu>
2017-10-26 01:35:35 -0600

    Changed WW1 to the one with score and added WW3

--- A4/MatLab/CS4300_RunTests.m
+++ A4/MatLab/CS4300_RunTests.m
@@ -2,16 +2,12 @@ function t = CS4300_RunTests()
 %CS4300_RUNTESTS Summary of this function goes here
 %   Detailed explanation goes here
 
-%     clear function CS4300_hybrid_agent;
-%     t=CS4300_WW1(50,'CS4300_hybrid_agent',[0,0,0,3;0,0,0,0;2,1,0,0;0,0,0,0]);
-%     CS4300_show_trace(t, 0.5);
-%     
-%     clear function CS4300_hybrid_agent;
-%     t=CS4300_WW1(50,'CS4300_hybrid_agent',[0,0,0,1;3,2,1,0;0,0,0,0;0,0,1,0]);
-%     CS4300_show_trace(t, 0.5);
-%     
     clear function CS4300_hybrid_agent;
-    t=CS4300_WW1(50,'CS4300_hybrid_agent',[0,0,0,0;0,0,0,0;3,2,0,0;0,1,0,0]);
-    CS4300_show_trace(t, 0.5);
+    [scores,traces]=CS4300_WW3(50,'CS4300_hybrid_agent');
+    %CS4300_show_trace(t, 1);
+    scores(1).score
+    scores(2).score
+    scores(3).score
+    CS4300_show_trace(traces, 0.5);
 end
 
