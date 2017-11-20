b3acacf99ccf3f850b677542288b68dcad6079d2
tiboedturtels <u0927597@utah.edu>
2017-11-20 15:36:20 -0700

    Added driver for the WW test

---
+++ A7/Matlab/CS4300_WW_run_value_iteration.m
@@ -0,0 +1,120 @@
+function [S,A,R,P,U,Ut] = CS4300_WW_run_value_iteration(gamma,max_iter)
+% CS4300_run_value_iteration - runs the value iteration for the given Wumpus
+% World Problem
+% On input: 
+%   gamma (float): discount factor
+%   max_iter (int): max number of iterations  
+% On output: 
+%   S (vector): states (1 to n) 
+%   A (vector): actions (1 to k)
+%   R (vector): state rewards 
+%   P (nxk struct array): transition model 
+%       (s,a).probs (a vector with n transition probabilities 
+%       (from s to s_prime, given action a) 
+%   U (vector): state utilities 
+%   U_trace (iterxn): trace of utility values during iteration 
+% Call: 
+%   [S,A,R,P,U,Ut] = CS4300_WW_run_value_iteration(0.999999,1000)
+% Author: 
+% William Garnes and Cameron Jackson 
+% UU 
+% Fall 2017 
+%
+
+S = 1:16;
+
+%Board Actions 
+UP = 1;
+DOWN = 2;
+LEFT = 3;
+RIGHT = 4;
+A = [UP, DOWN, LEFT, RIGHT];
+
+P(16,4).probs = zeros(1,16);
+
+num_states = length(S);
+num_actions = length(A);
+for state = 1 : num_states
+    for action = 1 : num_actions
+        [target, miss_left, miss_right] = CS4300_get_possible_cells(state,action);
+        P(state, action).probs = zeros(1,16);
+        P(state, action).probs(target) = P(state, action).probs(target) + 0.8;
+        P(state, action).probs(miss_left) = P(state, action).probs(miss_left) + 0.1;
+        P(state, action).probs(miss_right) = P(state, action).probs(miss_right) + 0.1;
+    end
+end
+            
+
+% %probabilities
+% P(1,UP).probs = [.1,.1,0,0, .8,0,0,0, 0,0,0,0, 0,0,0,0];
+% P(1,DOWN).probs = [.9,.1,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+% P(1,LEFT).probs = [.9,0,0,0,.1,0,0,0,0,0,0];
+% P(1,RIGHT).probs = [.1,.8,0,0,.1,0,0,0,0,0,0];
+% 
+% P(2,UP).probs = [.1,.8,.1,0,0,0,0,0,0,0,0];
+% P(2,DOWN).probs = [.1,.8,.1,0,0,0,0,0,0,0,0];
+% P(2,LEFT).probs = [.8,.2,0,0,0,0,0,0,0,0,0];
+% P(2,RIGHT).probs = [0,.2,.8,0,0,0,0,0,0,0,0];
+% 
+% P(3,UP).probs = [0,.1,0,.1,0,.8,0,0,0,0,0];
+% P(3,DOWN).probs = [0,.1,.8,.1,0,0,0,0,0,0,0];
+% P(3,LEFT).probs = [0,.8,.1,0,0,.1,0,0,0,0,0];
+% P(3,RIGHT).probs = [0,0,.1,.8,0,.1,0,0,0,0,0];
+% 
+% P(4,UP).probs = [0,0,.1,.1,0,0,.8,0,0,0,0];
+% P(4,DOWN).probs = [0,0,.1,.9,0,0,0,0,0,0,0];
+% P(4,LEFT).probs = [0,0,.8,.1,0,0,.1,0,0,0,0];
+% P(4,RIGHT).probs = [0,0,0,.9,0,0,.1,0,0,0,0];
+% 
+% P(5,UP).probs = [0,0,0,0,.2,0,0,.8,0,0,0];
+% P(5,DOWN).probs = [.8,0,0,0,.2,0,0,0,0,0,0];
+% P(5,LEFT).probs = [.1,0,0,0,.8,0,0,.1,0,0,0];
+% P(5,RIGHT).probs = [.1,0,0,0,.8,0,0,.1,0,0,0];
+% 
+% P(6,UP).probs = [0,0,0,0,0,.1,.1,0,0,.8,0];
+% P(6,DOWN).probs = [0,0,.8,0,0,.1,.1,0,0,0,0];
+% P(6,LEFT).probs = [0,0,.1,0,0,.8,0,0,0,.1,0];
+% P(6,RIGHT).probs = [0,0,.1,0,0,0,.8,0,0,.1,0];
+% 
+% % P(7,UP).probs = [0,0,0,0,0,.1,.1,0,0,0,.8];
+% % P(7,DOWN).probs = [0,0,0,.8,0,.1,.1,0,0,0,0];
+% % P(7,LEFT).probs = [0,0,0,.1,0,.8,0,0,0,0,.1];
+% % P(7,RIGHT).probs = [0,0,0,.1,0,0,.8,0,0,0,.1];
+% 
+% P(7,UP).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% P(7,DOWN).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% P(7,LEFT).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% P(7,RIGHT).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% 
+% P(8,UP).probs = [0,0,0,0,0,0,0,.9,.1,0,0];
+% P(8,DOWN).probs = [0,0,0,0,.8,0,0,.1,.1,0,0];
+% P(8,LEFT).probs = [0,0,0,0,.1,0,0,.9,0,0,0];
+% P(8,RIGHT).probs = [0,0,0,0,.1,0,0,.1,.8,0,0];
+% 
+% P(9,UP).probs = [0,0,0,0,0,0,0,.1,.8,.1,0];
+% P(9,DOWN).probs = [0,0,0,0,0,0,0,.1,.8,.1,0];
+% P(9,LEFT).probs = [0,0,0,0,0,0,0,.8,.2,0,0];
+% P(9,RIGHT).probs = [0,0,0,0,0,0,0,0,.2,.8,0];
+% 
+% P(10,UP).probs = [0,0,0,0,0,0,0,0,.1,.8,.1];
+% P(10,DOWN).probs = [0,0,0,0,0,.8,0,0,.1,0,.1];
+% P(10,LEFT).probs = [0,0,0,0,0,.1,0,0,.8,.1,0];
+% P(10,RIGHT).probs = [0,0,0,0,0,.1,0,0,0,.1,.8];
+% 
+% % P(11,UP).probs = [0,0,0,0,0,0,0,0,0,.1,.9];
+% % P(11,DOWN).probs = [0,0,0,0,0,0,.8,0,0,.1,.1];
+% % P(11,LEFT).probs = [0,0,0,0,0,0,.1,0,0,.8,.1];
+% % P(11,RIGHT).probs = [0,0,0,0,0,0,.1,0,0,0,.9];
+% 
+% P(11,UP).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% P(11,DOWN).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% P(11,LEFT).probs = [0,0,0,0,0,0,0,0,0,0,0];
+% P(11,RIGHT).probs = [0,0,0,0,0,0,0,0,0,0,0];
+
+%rewards
+REW = -1;
+R = [REW,REW,-1000,REW,  REW,REW,-1000,REW,  REW,REW,-1000,REW,  REW,REW,REW,1000];
+
+eta = .1;
+[U, Ut] = CS4300_MDP_value_iteration(S,A,P,R,gamma,eta,max_iter);
+policy = CS4300_MDP_policy(S,A,P,U);
